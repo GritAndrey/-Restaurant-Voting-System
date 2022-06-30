d3d098591cf65a027592375c1ee4409b4afcda55
Gritsunick Andrey <stendarr@gmail.com>
2022-06-30 19:08:47 +0300

    *work on the db and model.

--- src/main/java/ru/andreygri/restaurantvotingsystem/model/Restaurant.java
+++ src/main/java/ru/andreygri/restaurantvotingsystem/model/Restaurant.java
@@ -1,5 +1,17 @@
 package ru.andreygri.restaurantvotingsystem.model;
 
 public class Restaurant extends AbstractNamedEntity {
+    private String address;
 
+    public Restaurant(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
