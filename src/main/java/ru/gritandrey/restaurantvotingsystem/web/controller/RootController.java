76872123ba81be311ee19eb0ddd54c667444d1c4
Gritsunick Andrey <stendarr@gmail.com>
2022-08-19 03:41:16 +0300

    Added redirect. Added DockerFile and docker-compose.

---
+++ src/main/java/ru/gritandrey/restaurantvotingsystem/web/controller/RootController.java
@@ -0,0 +1,12 @@
+package ru.gritandrey.restaurantvotingsystem.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:swagger-ui/index.html";
+    }
+}
