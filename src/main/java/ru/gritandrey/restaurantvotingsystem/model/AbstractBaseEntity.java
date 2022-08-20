46d05178929ab3777ba368c9103f75c64224d3cf
Gritsunick Andrey <stendarr@gmail.com>
2022-08-21 01:49:24 +0300

    Changed strategy to GenerationType.IDENTITY No more queries like select nextval('global_seq')...

--- src/main/java/ru/gritandrey/restaurantvotingsystem/model/AbstractBaseEntity.java
+++ src/main/java/ru/gritandrey/restaurantvotingsystem/model/AbstractBaseEntity.java
@@ -8,19 +8,13 @@ import org.springframework.util.Assert;
 import javax.persistence.*;
 
 @MappedSuperclass
-// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
 public abstract class AbstractBaseEntity implements Persistable<Integer>, HasId {
     public static final int START_SEQ = 100000;
 
     @Id
-    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
-    //    @Column(name = "id", unique = true, nullable = false, columnDefinition = "integer default nextval('global_seq')")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-//  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
-//  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY) // https://stackoverflow.com/a/28025008/548473
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     protected Integer id;
 
     protected AbstractBaseEntity() {
